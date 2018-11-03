eba09c320ad1e6e45b49871467ccdd6f4484bded
Ahmed Hashem <me@ahashem.me>
2018-11-03 16:02:45 +0200

    Form Validation

--- script.js
+++ script.js
@@ -69,3 +69,36 @@ function formSubmit() {
 }
 
 document.querySelector("button").addEventListener('click', formSubmit, false);
+
+/*Form Validation*/
+
+var $form = $("form"),
+    $successMsg = $(".alert");
+$.validator.addMethod("letters", function(value, element) {
+    return this.optional(element) || value == value.match(/^[a-zA-Z\s]*$/);
+});
+$form.validate({
+    rules: {
+        fname: {
+            required: true,
+            minlength: 3,
+            letters: true
+        },
+        lname: {
+            required: true,
+            minlength: 3,
+            letters: true
+        },
+        email: {
+            required: true,
+            email: true
+        }
+    },
+    messages: {
+        name: "Please specify your name (only letters and spaces are allowed)",
+        email: "Please specify a valid email address"
+    },
+    submitHandler: function() {
+        $successMsg.show();
+    }
+});
